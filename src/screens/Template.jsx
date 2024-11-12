79d350f5916e76f7cdafdb73d8d957d84aa0feaf
Promise Duke <77851203+Promisead@users.noreply.github.com>
2024-11-12 09:14:17 +0100

    Update Template.jsx
    ai-link update

--- src/screens/Template.jsx
+++ src/screens/Template.jsx
@@ -167,7 +167,7 @@ const dashboardUrl = "https://crea8cv-v3.vercel.app"
           <Nav className="flex-column">
             <Nav.Link
               onClick={() => {
-                window.location.href ="https://ai-cv2.vercel.app/dashboard";
+                window.location.href ="https://ai-cv2.vercel.app";
               }}
               className={`text-white ${activeTab === "myCVs" ? "active" : ""}`}
             >
